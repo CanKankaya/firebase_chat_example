3bb4f7771db4fa39384e0323f14ef3af55cbd2e2
Can Kankaya <can.kankaya@outlook.com>
2022-08-09 18:21:52 +0300

    minor

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
