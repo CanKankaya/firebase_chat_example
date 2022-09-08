70b249bdf949f4823b6a95f8cbbce079ce8fe991
Can Kankaya <can.kankaya@outlook.com>
2022-09-08 19:02:29 +0300

    minor audio bug fix

--- lib/screens/audio_screen.dart
+++ lib/screens/audio_screen.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_chat_example/widgets/custom_icon_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 
@@ -10,15 +9,12 @@ import 'package:firebase_chat_example/widgets/app_drawer.dart';
 
 import 'package:firebase_chat_example/screens/mapstuff/no_internet_screen.dart';
 
-//TODO: Try audio stuff here
 class AudioScreen extends StatefulWidget {
   const AudioScreen({Key? key}) : super(key: key);
 
   @override
   State<AudioScreen> createState() => _AudioScreenState();
 }
-
-//TODO: Try multiple AudioPlayers with different sources
 
 class _AudioScreenState extends State<AudioScreen> {
   late AudioManager _audioManager;
@@ -38,92 +34,67 @@ class _AudioScreenState extends State<AudioScreen> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<bool>(
-        future: _audioManager.checkInternet(),
-        builder: (context, futureVal) {
-          if (futureVal.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (!(futureVal.data ?? false)) {
-            return NoInternetScreen();
-          }
-          return WillPopScope(
-            onWillPop: onWillPopHandler,
-            child: Scaffold(
-              key: _scaffoldKey,
-              appBar: AppBar(),
-              drawer: const AppDrawer(),
-              body: Column(
-                children: [
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: List.generate(
-                          urls.length,
-                          (index) => CustomPlayer(
-                            audioManager: _audioManager,
-                            index: index,
-                            url: urls[index],
-                          ),
+      future: _audioManager.checkInternet(),
+      builder: (context, futureVal) {
+        if (futureVal.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (!(futureVal.data ?? false)) {
+          return NoInternetScreen();
+        }
+        return WillPopScope(
+          onWillPop: onWillPopHandler,
+          child: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(),
+            drawer: const AppDrawer(),
+            body: Column(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: List.generate(
+                        urls.length,
+                        (index) => CustomPlayer(
+                          audioManager: _audioManager,
+                          index: index,
+                          url: urls[index],
                         ),
                       ),
                     ),
                   ),
-                  Row(
-                    children: [
-                      const Text('DEBUG BUTTON:'),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: ValueListenableBuilder<ButtonState>(
-                          valueListenable: _audioManager.buttonNotifier,
-                          builder: (_, value, __) {
-                            return ElevatedButton(
-                              onPressed: value == ButtonState.loading
-                                  ? null
-                                  : () {
-                                      _audioManager.dispose();
-                                      _audioManager.init();
-                                    },
-                              child: const Text('Reset audio test'),
-                            );
-                          },
-                        ),
+                ),
+                Row(
+                  children: [
+                    const Text('DEBUG BUTTON:'),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: ValueListenableBuilder<ButtonState>(
+                        valueListenable: _audioManager.buttonNotifier,
+                        builder: (_, value, __) {
+                          return ElevatedButton(
+                            onPressed: value == ButtonState.loading
+                                ? null
+                                : () {
+                                    _audioManager.dispose();
+                                    _audioManager.init();
+                                  },
+                            child: const Text('Reset audio test'),
+                          );
+                        },
                       ),
-                      const SizedBox(width: 8)
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Text('DEBUG BUTTON:'),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: ValueListenableBuilder<ButtonState>(
-                          valueListenable: _audioManager.buttonNotifier,
-                          builder: (_, value, __) {
-                            return ElevatedButton(
-                              onPressed: value == ButtonState.loading
-                                  ? null
-                                  : () {
-                                      //TODO: Change url here
-                                      _audioManager.changeUrl(
-                                        'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3',
-                                        0,
-                                      );
-                                    },
-                              child: const Text('Change audio test'),
-                            );
-                          },
-                        ),
-                      ),
-                      const SizedBox(width: 8)
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                    const SizedBox(width: 8)
+                  ],
+                ),
+              ],
             ),
-          );
-        });
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -225,15 +196,6 @@ class CustomPlayer extends StatelessWidget {
                                       position: position,
                                       urlToChange: url,
                                     );
-
-                                    SchedulerBinding.instance.addPostFrameCallback((_) {
-                                      ScaffoldMessenger.of(context).clearSnackBars();
-                                      ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                          content: Text(audioManager.lastActiveIndex.toString()),
-                                        ),
-                                      );
-                                    });
                                   },
                             progressBarColor: Colors.amber,
                             thumbColor: Colors.amber,
@@ -243,102 +205,71 @@ class CustomPlayer extends StatelessWidget {
                         },
                       ),
                       ValueListenableBuilder<bool>(
-                          valueListenable: audioManager.initIcon,
-                          builder: (context, initValue, __) {
-                            return ValueListenableBuilder<int>(
-                                valueListenable: audioManager.lastActiveIndex,
-                                builder: (context, indexValue, __) {
-                                  return ValueListenableBuilder<ButtonState>(
-                                    valueListenable: audioManager.buttonNotifier,
-                                    builder: (_, value, __) {
-                                      if (audioManager.lastActiveIndex.value == index) {
-                                        return value == ButtonState.loading
-                                            ? const Padding(
-                                                padding: EdgeInsets.all(6.0),
-                                                child: CircularProgressIndicator(),
-                                              )
-                                            : CustomIconButton(
-                                                icon: initValue
-                                                    ? AnimatedIcons.play_pause
-                                                    : AnimatedIcons.pause_play,
-                                                buttonFon: audioManager.isPlaying
-                                                    ? () {
-                                                        audioManager.isPlaying = false;
-                                                        audioManager.pause();
-                                                      }
-                                                    : () async {
-                                                        audioManager.isPlaying = true;
-                                                        audioManager.play(index);
-                                                        SchedulerBinding.instance
-                                                            .addPostFrameCallback((_) {
-                                                          ScaffoldMessenger.of(context)
-                                                              .clearSnackBars();
-                                                          ScaffoldMessenger.of(context)
-                                                              .showSnackBar(
-                                                            SnackBar(
-                                                              content: Text(audioManager
-                                                                  .lastActiveIndex.value
-                                                                  .toString()),
-                                                            ),
-                                                          );
-                                                        });
-                                                      },
-                                              );
-                                      } else {
-                                        switch (value) {
-                                          case ButtonState.loading:
-                                            return IconButton(
-                                              onPressed: () {},
-                                              icon: const Icon(Icons.play_arrow),
-                                              iconSize: 24,
-                                            );
-                                          case ButtonState.paused:
-                                            return IconButton(
-                                              onPressed: () async {
-                                                audioManager.isPlaying = true;
-                                                await audioManager.changeUrl(url, index);
-                                                audioManager.play(index);
-                                                SchedulerBinding.instance.addPostFrameCallback((_) {
-                                                  ScaffoldMessenger.of(context).clearSnackBars();
-                                                  ScaffoldMessenger.of(context).showSnackBar(
-                                                    SnackBar(
-                                                      content: Text(audioManager
-                                                          .lastActiveIndex.value
-                                                          .toString()),
-                                                    ),
-                                                  );
-                                                });
-                                              },
-                                              icon: const Icon(Icons.play_arrow),
-                                              iconSize: 24,
-                                            );
+                        valueListenable: audioManager.initIcon,
+                        builder: (context, initValue, __) {
+                          return ValueListenableBuilder<int>(
+                            valueListenable: audioManager.lastActiveIndex,
+                            builder: (context, indexValue, __) {
+                              return ValueListenableBuilder<ButtonState>(
+                                valueListenable: audioManager.buttonNotifier,
+                                builder: (_, value, __) {
+                                  if (audioManager.lastActiveIndex.value == index) {
+                                    return value == ButtonState.loading
+                                        ? const Padding(
+                                            padding: EdgeInsets.all(6.0),
+                                            child: CircularProgressIndicator(),
+                                          )
+                                        : CustomIconButton(
+                                            icon: initValue
+                                                ? AnimatedIcons.play_pause
+                                                : AnimatedIcons.pause_play,
+                                            buttonFon: audioManager.isPlaying
+                                                ? () {
+                                                    audioManager.isPlaying = false;
+                                                    audioManager.pause();
+                                                  }
+                                                : () async {
+                                                    audioManager.isPlaying = true;
+                                                    audioManager.play(index);
+                                                  },
+                                          );
+                                  } else {
+                                    switch (value) {
+                                      case ButtonState.loading:
+                                        return IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(Icons.play_arrow),
+                                          iconSize: 24,
+                                        );
+                                      case ButtonState.paused:
+                                        return IconButton(
+                                          onPressed: () async {
+                                            audioManager.isPlaying = true;
+                                            await audioManager.changeUrl(url, index);
+                                            audioManager.play(index);
+                                          },
+                                          icon: const Icon(Icons.play_arrow),
+                                          iconSize: 24,
+                                        );
 
-                                          case ButtonState.playing:
-                                            return IconButton(
-                                              onPressed: () async {
-                                                audioManager.isPlaying = true;
-                                                await audioManager.changeUrl(url, index);
-                                                audioManager.play(index);
-                                                SchedulerBinding.instance.addPostFrameCallback((_) {
-                                                  ScaffoldMessenger.of(context).clearSnackBars();
-                                                  ScaffoldMessenger.of(context).showSnackBar(
-                                                    SnackBar(
-                                                      content: Text(audioManager
-                                                          .lastActiveIndex.value
-                                                          .toString()),
-                                                    ),
-                                                  );
-                                                });
-                                              },
-                                              icon: const Icon(Icons.play_arrow),
-                                              iconSize: 24,
-                                            );
-                                        }
-                                      }
-                                    },
-                                  );
-                                });
-                          }),
+                                      case ButtonState.playing:
+                                        return IconButton(
+                                          onPressed: () async {
+                                            audioManager.isPlaying = true;
+                                            await audioManager.changeUrl(url, index);
+                                            audioManager.play(index);
+                                          },
+                                          icon: const Icon(Icons.play_arrow),
+                                          iconSize: 24,
+                                        );
+                                    }
+                                  }
+                                },
+                              );
+                            },
+                          );
+                        },
+                      ),
                     ],
                   );
                 },
